169e419d6615ee752ca87ac188c546c6f4bce061
Anurag <anuragmarketing101@gmail.com>
2023-02-05 14:11:57 +0530

    taskBar-icons-updated

--- components/TaskBar/TaskBar.tsx
+++ components/TaskBar/TaskBar.tsx
@@ -1,7 +1,17 @@
-import { TaskBarIconsDataArray } from '@/constants/TaskBarIconsData/TaskBarIconsData'
+"use client"
+
 import React from 'react'
-import { FaSearch } from "react-icons/fa"
+import { TaskBarIconsDataArray } from '@/constants/TaskBarIconsData/TaskBarIconsData'
 import TaskBarIcon from '../TaskBarIcon/TaskBarIcon'
+
+// Icons Import
+import { FaSearch } from "react-icons/fa"
+import { BsHammer, BsPersonFill, BsInstagram, BsTwitter, BsLinkedin, BsYoutube } from "react-icons/bs"
+import { GiSkills } from "react-icons/gi"
+import { MdMail } from "react-icons/md"
+import { RiMusic2Fill } from "react-icons/ri"
+import { IoMdDownload } from "react-icons/io"
+
 
 const TaskBar = () => {
   return (
@@ -9,18 +19,113 @@ const TaskBar = () => {
       <div className='w-full h-full -mt-2 -ml-2 bg-[#F7CF59] border border-black flex justify-start items-center rounded-lg space-x-2 sm:space-x-5 px-5'>
 
         {/* Search Icon */}
-
-        <div className={`w-14 h-14 flex justify-center items-center bg-black border border-black rounded-md`}>
-          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#39DBFF] text-white rounded-md`}>
-            <FaSearch />
+        <div className={`w-14 h-14 flex justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#39DBFF] text-white border-2 border-black`}>
+            <FaSearch className='text-black' size={"1.5rem"} />
           </div>
         </div>
 
         <div className='w-[2px] h-full border-r-2 border-r-black'></div>
 
-        {TaskBarIconsDataArray && TaskBarIconsDataArray?.map((icon) => {
+        {/* {TaskBarIconsDataArray && TaskBarIconsDataArray?.map((icon) => {
           return <TaskBarIcon key={icon.id} icon={icon} />
-        })}
+        })} */}
+
+        {/* Skills icon */}
+        <div className={`lg:hidden w-14 h-14 flex justify-center items-center bg-black border border-black`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#FFFFB0] text-white border-2 border-black`}>
+            <GiSkills className='text-black' size={"1.5rem"} />
+          </div>
+        </div>
+
+        {/* Projects icon */}
+        <div className={`lg:hidden w-14 h-14 flex justify-center items-center bg-black border border-black`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#F9A47B] text-white border-2 border-black`}>
+            <BsHammer className='text-black' size={"1.5rem"} />
+          </div>
+        </div>
+
+        {/* About icon */}
+        <div className={`lg:hidden w-14 h-14 flex justify-center items-center bg-black border border-black`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-white text-white border-2 border-black`}>
+            <BsPersonFill className='text-black' size={"1.5rem"} />
+          </div>
+        </div>
+
+        {/* Contact icon */}
+        <div className={`lg:hidden w-14 h-14 flex justify-center items-center bg-black border border-black`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#E7E7E7] text-white border-2 border-black`}>
+            <MdMail className='text-black' size={"1.5rem"} />
+          </div>
+        </div>
+
+        {/* Download icon */}
+        <div className={`lg:hidden w-14 h-14 flex justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#00FD74] text-white border-2 border-black`}>
+            <IoMdDownload className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+
+
+
+        {/* Music icon */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#FD5D5D] text-white border-2 border-black`}>
+            <RiMusic2Fill className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+        {/* Contact icon */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#FDF300] text-white border-2 border-black`}>
+            <MdMail className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+        {/* Download icon */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#00FD74] text-white border-2 border-black`}>
+            <IoMdDownload className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+        {/* About icon */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#DB7CFD] text-white border-2 border-black`}>
+            <BsPersonFill className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+        {/* Instagram icon */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#DB3073] text-white border-2 border-black`}>
+            <BsInstagram className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+
+        {/* Twitter icon */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#1DA1F2] text-white border-2 border-black`}>
+            <BsTwitter className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+
+        {/* Linkedin icon  */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#007BB6] text-white border-2 border-black`}>
+            <BsLinkedin className='text-black' size={"2rem"} />
+          </div>
+        </div>
+
+        {/* Youtube icon  */}
+        <div className={`hidden lg:inline-flex w-14 h-14 justify-center items-center bg-black border border-black hover:scale-110 transition-all duration-150 ease-in-out`}>
+          <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center bg-[#FE0002] text-white border-2 border-black `}>
+            <BsYoutube className='text-black' size={"2rem"} />
+          </div>
+        </div>
 
       </div>
     </div>
--- components/TaskBarIcon/TaskBarIcon.tsx
+++ components/TaskBarIcon/TaskBarIcon.tsx
@@ -8,6 +8,7 @@ import { BsFillPersonFill, BsInstagram, BsTwitter, BsYoutube } from "react-icons
 import { ImLinkedin2 } from "react-icons/im"
 import {RiMusic2Fill} from "react-icons/ri"
 
+// hover:-mt-0 hover:-ml-0 
 
 interface IProps {
     icon: ITaskBarIconsData
@@ -15,8 +16,8 @@ interface IProps {
 
 const TaskBarIcon = ({ icon }: IProps) => {
     return (
-        <div onClick={() => console.log(icon)} className={` ${icon.isVisibleOnMobile ? "inline-flex" : "hidden sm:inline-flex"} w-14 h-14  justify-center items-center bg-black border border-black rounded-md`}>
-            <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center rounded-md 
+        <div onClick={() => console.log(icon)} className={` ${icon.isVisibleOnMobile ? "inline-flex" : "hidden sm:inline-flex"} w-14 h-14 justify-center items-center bg-black border border-black  hover:scale-110 `}>
+            <div className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center border-2 border-black
             ${icon.icon === "IoMdDownload" && "bg-[#00FD74]"} 
             ${icon.icon === "MdMail" && "bg-[#FEF400]"}  
             ${icon.icon === "BsFillPersonFill" && "bg-[#DC7CFE]"}  
@@ -29,14 +30,14 @@ const TaskBarIcon = ({ icon }: IProps) => {
             `}>
 
                 {/* {icon?.icon} */}
-                {icon.icon === "IoMdDownload" && <IoMdDownload className='text-black' size={"1.5rem"} />}
-                {icon.icon === "MdMail" && <MdMail className='text-black' size={"1.5rem"} />}
-                {icon.icon === "BsFillPersonFill" && <BsFillPersonFill className='text-black' size={"1.5rem"} />}
-                {icon.icon === "ImLinkedin2" && <ImLinkedin2 className='text-black'size={"1.5rem"} />}
-                {icon.icon === "BsInstagram" && <BsInstagram className='text-black'size={"1.5rem"} />}
-                {icon.icon === "BsTwitter" && <BsTwitter className='text-black'size={"1.5rem"} />}
-                {icon.icon === "BsYoutube" && <BsYoutube className='text-black'size={"1.5rem"} />}
-                {icon.icon === "RiMusic2Fill" && <RiMusic2Fill className='text-black'size={"1.5rem"} />}
+                {icon.icon === "IoMdDownload" && <IoMdDownload className='text-black' size={"2rem"} />}
+                {icon.icon === "MdMail" && <MdMail className='text-black' size={"2rem"} />}
+                {icon.icon === "BsFillPersonFill" && <BsFillPersonFill className='text-black' size={"2rem"} />}
+                {icon.icon === "ImLinkedin2" && <ImLinkedin2 className='text-black'size={"2rem"} />}
+                {icon.icon === "BsInstagram" && <BsInstagram className='text-black'size={"2rem"} />}
+                {icon.icon === "BsTwitter" && <BsTwitter className='text-black'size={"2rem"} />}
+                {icon.icon === "BsYoutube" && <BsYoutube className='text-black'size={"2rem"} />}
+                {icon.icon === "RiMusic2Fill" && <RiMusic2Fill className='text-black'size={"2rem"} />}
             </div>
         </div >
     )
