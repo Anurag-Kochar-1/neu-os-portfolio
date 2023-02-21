516bcd7516a34c2ee00f3b3376bfef719a2f3141
Anurag <anuragmarketing101@gmail.com>
2023-02-21 13:41:26 +0530

    folder-redirect-test-aded

--- components/FolderCard/FolderCard.tsx
+++ components/FolderCard/FolderCard.tsx
@@ -6,7 +6,7 @@ import folderIcon from "../../public/images/folderIcon.png"
 import Image from 'next/image'
 import { AppContext, IAppContextType } from '@/context/AppContext'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'
 
 
 interface IProps {
@@ -16,12 +16,17 @@ interface IProps {
 
 const FolderCard = ({ folder }: IProps) => {
   const { isFolderOpen, setIsFolderOpen } = useContext<IAppContextType>(AppContext)
-  const pathname = usePathname();
+
+  const searchParams = useSearchParams();
+
+  const folderName = searchParams.get('folder');
 
   return (
     <Link
       href={`?folder=${folder.folderName}`}
-      onClick={() => setIsFolderOpen(!isFolderOpen)}
+      onClick={() => {
+        setIsFolderOpen(!isFolderOpen)
+      }}
       className='flex flex-col justify-start items-center space-y-2 m-1 hover:scale-105'>
       <Image src={folderIcon} alt={folder.folderName} width={400} height={400} className="w-20 xl:w-32" draggable="false" />
       <h3 className='text-black text-xl font-bold'> {folder.folderName} </h3>
