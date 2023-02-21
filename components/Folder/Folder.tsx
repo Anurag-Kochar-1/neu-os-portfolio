0270518b65cdd81925a908e426c70c20bb6d4213
Anurag <anuragmarketing101@gmail.com>
2023-02-21 12:30:18 +0530

    draggable-folder-added

--- components/Folder/Folder.tsx
+++ components/Folder/Folder.tsx
@@ -1,5 +1,7 @@
-import { FolderContext } from '@/context/FolderContext/FolderContext'
 import React, { useContext } from 'react'
+import { FolderContext } from '@/context/FolderContext/FolderContext'
+import Draggable from 'react-draggable';
+
 
 const Folder = () => {
     const { isFolderOpen, setIsFolderOpen }: any = useContext(FolderContext)
@@ -7,14 +9,21 @@ const Folder = () => {
 
     if (!isFolderOpen) return null
     return (
-        <div className='z-30 fixed inset-0 w-full h-screen bg-black/[.40] flex justify-center items-center'>
-            <div className='w-[80%] h-[80vh] bg-white'>
-                <strong
-                    className='text-3xl text-red-600'
-                    onClick={() => setIsFolderOpen(!isFolderOpen)}> X
-                </strong>
-            </div>
 
+        <div className='z-30 fixed inset-0 w-full h-screen bg-black/[.40] flex justify-center items-center'>
+            <Draggable
+                handle='.headerHandle'
+            >
+                <div className='w-[80%] h-[80vh] bg-white'>
+                    <div className='headerHandle w-full h-12 bg-black flex justify-between items-center'>
+                        <strong
+                            className='text-3xl text-red-600'
+                            onClick={() => setIsFolderOpen(!isFolderOpen)}> X
+                        </strong>
+                    </div>
+                </div>
+
+            </Draggable>
         </div>
     )
 }
