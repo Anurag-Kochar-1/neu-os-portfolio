59846f472aa04cf114ff7a9d1eb599a1ef0d2fc5
Anurag <anuragmarketing101@gmail.com>
2023-04-17 21:22:07 +0530

    Fixed: TaskBarIcon bg color

--- components/TaskBar/TaskBar.tsx
+++ components/TaskBar/TaskBar.tsx
@@ -7,6 +7,32 @@ import TaskBarIcon from "../TaskBarIcon/TaskBarIcon";
 const TaskBar = () => {
   const iconRef = useRef<HTMLDivElement | null>(null);
 
+  const getTaskBarIconBgColor = (iconName: string) => {
+    switch (iconName) {
+      case `Search`:
+        return `bg-[#39DBFF]`;
+      case `Music`:
+        return `bg-[#FE5E5E]`;
+      case `Mail`:
+        return `bg-[#FEF400]`;
+      case `Download Resume`:
+        return `bg-[#00FE75]`;
+      case `About`:
+        return `bg-[#DB7CFD]`;
+      case `Instagram`:
+        return `bg-[#DB3073]`;
+      case `Twitter`:
+        return `bg-[#1DA1F2]`;
+      case `YouTube`:
+        return `bg-[#FE0002]`;
+      case `LinkedIn`:
+        return `bg-[#007BB6]`;
+
+      default:
+        return `bg-white`;
+    }
+  };
+
   return (
     <div className="z-30 fixed bottom-2 md:bottom-[7vh] max-w-[97%] md:w-auto h-20 bg-black border border-black flex justify-center items-center rounded-lg">
       <div
@@ -24,10 +50,14 @@ const TaskBar = () => {
                 } w-12 h-12  justify-center items-center bg-black border border-black hover:cursor-pointer hover:scale-110 transition-all duration-150 ease-in-out`}
               >
                 <div
-                  className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center text-white border-2 border-black pointer-events-none ${taskBarIcon.bgColor}
+                  className={`w-full h-full -mt-2 -ml-2 flex justify-center items-center text-white border-2 border-black pointer-events-none ${getTaskBarIconBgColor(
+                    taskBarIcon.iconName
+                  )}
               `}
                 >
-                  <span className={`${taskBarIcon.iconColor} text-xl md:text-3xl pointer-events-none`}>
+                  <span
+                    className={`${taskBarIcon.iconColor} text-xl md:text-3xl pointer-events-none`}
+                  >
                     {taskBarIcon.icon}
                   </span>
                 </div>
