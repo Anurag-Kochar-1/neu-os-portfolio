7e95043c5130b2d09fec9906132245ccbbd77770
Anurag <anuragmarketing101@gmail.com>
2023-04-21 21:31:08 +0530

    Fixed: Project Content

--- components/FoldersContent/ProjectContent.tsx
+++ components/FoldersContent/ProjectContent.tsx
@@ -1,18 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { ProjectsData } from "@/constants/data/ProjectsData/ProjectsData";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import { IProject } from "@/types/ProjectData";
 
 const ProjectContent = () => {
+  const [projectData, setProjectData] = useState <IProject | null> (null);
   const searchParams = useSearchParams();
   const subFolderName = searchParams.get("subFolder");
 
-  if(!subFolderName) return null;
+  function getProjectData() {
+    const project = ProjectsData?.find(
+      (x) => x.projectName === subFolderName
+    );
+    if(project) setProjectData(project)
+  }
+
+  useEffect(() => {
+    getProjectData();
+  }, [subFolderName]);
+
+  if (!subFolderName) return null;
   return (
-    <h1 className="text-5xl font-bold" onClick={() => {
-        null
-    }}>
-        {subFolderName}
+    <h1
+      className="text-5xl font-bold"
+      onClick={() => {
+        null;
+      }}
+    >
+      {JSON.stringify(projectData?.liveLink)}
     </h1>
-  )
+  );
 };
 
 export default ProjectContent;
