ea83180e9dbd3d8146b986eede8aee70978063d4
Anurag <anuragmarketing101@gmail.com>
2023-04-22 14:44:19 +0530

    Added: TEMP Contact and About Folder Content

--- components/Folder/Folder.tsx
+++ components/Folder/Folder.tsx
@@ -65,6 +65,8 @@ const Folder = () => {
     }
   };
 
+  const ulClass = `text-base text-black font-medium` 
+
   if (!folderName) return null;
 
   return (
@@ -93,9 +95,34 @@ const Folder = () => {
           {subFolderName ? <ProjectContent /> : null}
 
           {!availableFolders?.includes(folderName) && (
-            <p className="w-full text-center my-10 text-5xl font-semibold">
-              This Folder is under construction
-            </p>
+            <div className="w-full  my-10 d flex flex-col justify-center items-center space-y-3">
+              <p className="text-center text-5xl font-semibol">
+                {folderName} Folder is under construction
+              </p>
+              {folderName === "Contact" && (
+                <p className="text-2xl font-medium">
+                  {" "}
+                  Email: anuragmarketing101@gmail.com{" "}
+                </p>
+              )}
+
+              {folderName === "About" && (
+                <div className="flex flex-col items-start justify-start space-y-2">
+                  <p className={`text-3xl font-semibold mb-5`}> But, here is the little summary </p>
+                  <p className={ulClass}> • For Almost 3 years = Did game live streaming on some multiple platforms </p>
+                  <p className={ulClass}> • 2021 = Completed my 12th ("yes I was in the covid batch") </p>
+                  <p className={ulClass}> • Childhood - 2021  {"=>"} Architecture Enthusiast {"=>"} Joined BDes Interior Design in Oct 2021 </p>
+                  <p className={ulClass}> • Dropped out of college in 2nd sem </p>
+                  <p className={ulClass}> • Built a Startup </p>
+                  <p className={ulClass}> • Created MVP - <a className="text-blue-500 hover:cursor-pointer" href={"https://offsta-mvp-v2-ak-1.bubbleapps.io/version-test/place_entrance_screen/1655294587384x550707056462528500"} target="_blank" rel="noreferrer"> MVP Link </a>  </p>
+                  <p className={ulClass}> • Tested with some users </p>
+                  <p className={ulClass}> • Shut down the Startup after 7 months :D </p>
+                  <p className={ulClass}> • Learnt web dev ("took almost 6-7 months") </p>
+                  <p className={ulClass}> • March 2023 = Joined a BCA Online program from a decent college </p>
+                  <p className={ulClass}> • April 2023 =  Currently doing an Internship as Frontend Developer  </p>
+                </div>
+              )}
+            </div>
           )}
         </div>
       </Draggable>
